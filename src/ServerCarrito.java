62b5277837e4563747d31a08f3666c781a68a6af
Roberto-coder <61215751+Roberto-coder@users.noreply.github.com>
2024-05-15 22:00:35 -0600

    pa q compile el server

--- src/ServerCarrito.java
+++ src/ServerCarrito.java
@@ -14,7 +14,7 @@ public class ServerCarrito {
 
         try {//Iniciamos proceso de recepcion de archivo serializado
             int port=6030;
-            InetAddress address = InetAddress.getByName("192.168.137.1");
+            InetAddress address = InetAddress.getByName("192.168.1.91");
             ServerSocket s = new ServerSocket(port, 50, address);
             //ServerSocket s = new ServerSocket(port);//Socket en el puerto 6030
             System.out.println("Servidor iniciado en " + address.getHostAddress() + ":" + port);
@@ -57,12 +57,6 @@ public class ServerCarrito {
             fileOut.close();
             System.out.println("Catalogo creado");
 
-            // Deserializando el ArrayList
-            FileInputStream fileIn = new FileInputStream("src/Servidor/catalogo.txt");
-            ObjectInputStream in = new ObjectInputStream(fileIn);
-            ArrayList<String> listDeserialized = (ArrayList<String>) in.readObject();
-            in.close();
-            fileIn.close();
         } catch (Exception i) {
             System.out.println(i);
         }
@@ -169,6 +163,7 @@ public class ServerCarrito {
 
         return nombre;
     }
+    @SuppressWarnings("unchecked")
     public static void mostrarProductos() {
         System.out.println("Mostrando productos...");
         try{
